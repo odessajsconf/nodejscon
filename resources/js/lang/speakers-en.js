0a6a95ce1abb0207b2c7a9ba5a8ce2a4ee41e3e3
mkatrichenko <mkatrichenko@ukr.net>
2018-08-22 23:26:40 +0300

    speakers update; early bird str delete

---
+++ resources/js/lang/speakers-en.js
@@ -0,0 +1,206 @@
+export const SpeakersEn = [
+  // {
+  //   image : '../public/img/speakers/nikita_galkin.jpg',
+  //   name : 'Nikita Galkin',
+  //   position : 'System Architect',
+  //   company : 'HighLoad projects',
+  //   rept : [
+  //     {
+  //       title : 'Node.js and Browser: which protocol to choose [Ru]',
+  //       description : 'Доклад начнется с разбора сильных и слабых сторон Node.js. Так же мы вспомним базовые концепции EventEmitter, Stream и EventLoop. Далее будут рассмотрены протоколы, по которым в современном вебе передаются данные: REST, WebSockets, ServerSideEvents, GraphQL. В заключение будет протокол агностик структура приложения.'
+  //     }
+  //   ],
+  //   aboutSpeaker : '',
+  //   socialsRendered : '',
+  //   socials : [
+  //     {
+  //       link : 'https://twitter.com/galk_in',
+  //       fatype : 'twitter'
+  //     },
+  //     {
+  //       link : 'https://www.linkedin.com/in/nikitagalkin/',
+  //       fatype : 'linkedin'
+  //     },
+  //     {
+  //       link : 'https://github.com/galkin',
+  //       fatype : 'github'
+  //     },
+  //   ]
+  // },
+  {
+    image : '../public/img/speakers/max_klymyshyn.jpg',
+    name : 'Max Klymyshyn',
+    position : 'Tech Lead',
+    company : 'Takeoff Technologies',
+    rept : [
+      {
+        title : 'Conflict-free replicated data types: distributed data in detail [RU] [Talk]',
+        description : 'Distributed systems - how to arrange and how you can practically apply ' +
+        'distributed data structures based on ideas from the field of CRDT'
+      },
+      {
+        title : 'Build blockchain using CRDT and Merkle Trees [Ru] [Workshop]',
+        description : '<ul>\n' +
+        '  <li>introduction &amp; basic tools installation</li>\n' +
+        '  <li>blockchain basic implementation</li>\n' +
+        '  <li>low-level transport</li>\n' +
+        '  <li>high-level transport &amp; CRDT payload definition</li>\n' +
+        '  <li>adding distributed properties</li>\n' +
+        '</ul>'
+      }
+    ],
+    aboutSpeaker :
+    'Full-stack software engineer for Clojure/ClojureScript/Python/JavaScript-based projects with 15+ years experience in technical' +
+    ' team leadership and management of distributed teams. Max interested in distributed systems, data replication and consistency algorithms, ' +
+    'information science, functional languages and modern mobile and front-end development. Delivered more than 50 talks within past 5 years ' +
+    'about developments and trends in Python, JavaScript, Databases, Dev Processes, Testing Processes and Project Management.',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://www.linkedin.com/in/klymyshyn',
+        fatype : 'linkedin'
+      },
+      {
+        link : 'https://twitter.com/maxmaxmaxmax',
+        fatype : 'twitter'
+      }
+    ]
+  },
+  {
+    image : '../public/img/speakers/roman_sachenko.jpg',
+    name : 'Roman Sachenko',
+    position : 'Software Engineer',
+    company : 'DA-14',
+    rept : [
+      {
+        title : 'Security in NodeJS [Ru]',
+        description : ''
+      },
+      {
+        title : 'App diagnostics under the hood [Ru]',
+        description : ''
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://github.com/roman-sachenko',
+        fatype : 'github'
+      },
+      /*{
+       link: 'https://www.linkedin.com/in/rsachenko/',
+       fatype: 'linkedin-square'
+       },
+       {
+       link: 'https://www.facebook.com/rsachenko',
+       fatype: 'facebook'
+       },*/
+      {
+        link : 'https://twitter.com/RSachenko',
+        fatype : 'twitter'
+      },
+      {
+        link : 'https://stackoverflow.com/users/5132363/roman-sachenko',
+        fatype : 'stack-overflow'
+      }/*,
+       {
+       link: 'https://www.instagram.com/rsachenko/',
+       fatype: 'instagram'
+       },*/
+    ]
+  },
+  // {
+  //   image : '../public/img/speakers/evgeniy_obrezkov.jpg',
+  //   name : 'Eugene Obrezkov',
+  //   position : 'Senior Software Engineer',
+  //   company : 'elastic.io',
+  //   rept : [
+  //     {
+  //       title : '',
+  //       description : ''
+  //     }
+  //   ],
+  //   aboutSpeaker : '',
+  //   socialsRendered : '',
+  //   socials : [
+  //     {
+  //       link : 'https://twitter.com/ghaiklor',
+  //       fatype : 'twitter'
+  //     },
+  //     {
+  //       link : 'https://www.facebook.com/ghaiklor?ref=br_rs',
+  //       fatype : 'facebook'
+  //     },
+  //     {
+  //       link : 'https://github.com/ghaiklor"',
+  //       fatype : 'github'
+  //     }
+  //   ]
+  // },
+  {
+    image : '../public/img/speakers/dmitriy_gusev.jpg',
+    name : 'Dmitriy Gusev',
+    position : 'Senior Frontend Developer',
+    company : '',
+    rept : [
+      {
+        title : '"Не бей лежачего" - полезные инструменты для быстрого старта разработки проекта на Node.JS [Ru]',
+        description : 'В ходе беседы поговорим о некоторых весьма полезных инструментах, для генерации стартового ' +
+        'состояние проекта, автоматической настройки основных компонентов. В общем, как автоматизировать все то, что' +
+        ' каждый раз приходится делать, но не очень хочется'
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://www.linkedin.com/in/dmitriy-gusev/',
+        fatype : 'linkedin'
+      },
+      {
+        link : 'https://github.com/dogusev"',
+        fatype : 'github'
+      }
+    ]
+  },
+  {
+	image: '../public/img/speakers/alexandra_kalinina.jpg',
+	name : 'Alexandra Kalinina',
+	position : '',
+	company : '',
+	rept : [
+		{
+			title : 'Testing of deployment process to GCP [Ru]',
+			description : ''
+		}
+	],
+	aboutSpeaker : '',
+    socialsRendered : '',
+	socials : []
+  },
+  {
+	image: '../public/img/speakers/dmitry_chirkin.jpg',
+	name : 'Dmitry Chirkin',
+	position : 'Web Developer',
+	company : 'Nix solutions',
+	rept : [
+		{
+			title : 'PostgreSQL для хипстеров или почему ваш следующий проект должен быть на PostgreSQL [Ru]',
+			description : ''
+		}
+	],
+	aboutSpeaker : '',
+    socialsRendered : '',
+	socials : [
+		{
+			link : 'https://github.com/josser',
+			fatype : 'github'
+		},
+		{
+          link : 'https://facebook.com/josser',
+          fatype : 'facebook'
+        },
+	]
+  }
+];
---
+++ resources/js/lang/speakers-ru.js
@@ -0,0 +1,206 @@
+export const SpeakersRu = [
+  // {
+  //   image : 'public/img/speakers/nikita_galkin.jpg',
+  //   name : 'Никита Галкин',
+  //   position : 'System Architect',
+  //   company : 'HighLoad projects',
+  //   rept : [
+  //     {
+  //       title : 'Node.js и браузер: какой протокол выбрать [Ru]',
+  //       description : 'Доклад начнется с разбора сильных и слабых сторон Node.js. Так же мы вспомним базовые концепции EventEmitter, Stream и EventLoop. Далее будут рассмотрены протоколы, по которым в современном вебе передаются данные: REST, WebSockets, ServerSideEvents, GraphQL. В заключение будет протокол агностик структура приложения.'
+  //     }
+  //   ],
+  //   aboutSpeaker : '',
+  //   socialsRendered : '',
+  //   socials : [
+  //     {
+  //       link : 'https://twitter.com/galk_in',
+  //       fatype : 'twitter'
+  //     },
+  //     {
+  //       link : 'https://www.linkedin.com/in/nikitagalkin/',
+  //       fatype : 'linkedin'
+  //     },
+  //     {
+  //       link : 'https://github.com/galkin',
+  //       fatype : 'github'
+  //     },
+  //   ]
+  // },
+  {
+    image : 'public/img/speakers/max_klymyshyn.jpg',
+    name : 'Максим Климишин',
+    position : 'Tech Lead',
+    company : 'Takeoff Technologies',
+    rept : [
+      {
+        title : 'Conflict-free replicated data types: распредедленные данные в деталях [RU] [Talk]',
+        description : 'Распределенные системы – как устроены и как можно практически применить ' +
+        'распределенные структуры данных на базе идей из области CRDT'
+      },
+      {
+        title : 'Построение блокчейна, используя CRDT и Merkle Trees [Ru] [Workshop]',
+        description : '<ul>\n' +
+        '  <li>introduction &amp; basic tools installation</li>\n' +
+        '  <li>blockchain basic implementation</li>\n' +
+        '  <li>low-level transport</li>\n' +
+        '  <li>high-level transport &amp; CRDT payload definition</li>\n' +
+        '  <li>adding distributed properties</li>\n' +
+        '</ul>'
+      },
+    ],
+    aboutSpeaker :
+    'Full-stack software engineer for Clojure/ClojureScript/Python/JavaScript-based projects with 15+ years experience in technical' +
+    ' team leadership and management of distributed teams. Max interested in distributed systems, data replication and consistency algorithms, ' +
+    'information science, functional languages and modern mobile and front-end development. Delivered more than 50 talks within past 5 years ' +
+    'about developments and trends in Python, JavaScript, Databases, Dev Processes, Testing Processes and Project Management.',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://www.linkedin.com/in/klymyshyn',
+        fatype : 'linkedin'
+      },
+      {
+        link : 'https://twitter.com/maxmaxmaxmax',
+        fatype : 'twitter'
+      }
+    ]
+  },
+  {
+    image : 'public/img/speakers/roman_sachenko.jpg',
+    name : 'Роман Саченко',
+    position : 'Software Engineer',
+    company : 'DA-14',
+    rept : [
+      {
+        title : 'Безопасность в Node.js [Ru]',
+        description : ''
+      },
+      {
+        title : 'App diagnostics under the hood [Ru]',
+        description : ''
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://github.com/roman-sachenko',
+        fatype : 'github'
+      },
+      /*{
+       link: 'https://www.linkedin.com/in/rsachenko/',
+       fatype: 'linkedin-square'
+       },
+       {
+       link: 'https://www.facebook.com/rsachenko',
+       fatype: 'facebook'
+       },*/
+      {
+        link : 'https://twitter.com/RSachenko',
+        fatype : 'twitter'
+      },
+      {
+        link : 'https://stackoverflow.com/users/5132363/roman-sachenko',
+        fatype : 'stack-overflow'
+      }/*,
+       {
+       link: 'https://www.instagram.com/rsachenko/',
+       fatype: 'instagram'
+       },*/
+    ]
+  },
+  // {
+  //   image : 'public/img/speakers/evgeniy_obrezkov.jpg',
+  //   name : 'Евгений Обрезков',
+  //   position : 'Senior Software Engineer',
+  //   company : 'elastic.io',
+  //   rept : [
+  //     {
+  //       title : '',
+  //       description : ''
+  //     }
+  //   ],
+  //   aboutSpeaker : '',
+  //   socialsRendered : '',
+  //   socials : [
+  //     {
+  //       link : 'https://twitter.com/ghaiklor',
+  //       fatype : 'twitter'
+  //     },
+  //     {
+  //       link : 'https://www.facebook.com/ghaiklor?ref=br_rs',
+  //       fatype : 'facebook'
+  //     },
+  //     {
+  //       link : 'https://github.com/ghaiklor"',
+  //       fatype : 'github'
+  //     },
+  //   ]
+  // },
+  {
+    image : 'public/img/speakers/dmitriy_gusev.jpg',
+    name : 'Дмитрий Гусев',
+    position : 'Senior Frontend Developer',
+    company : '',
+    rept : [
+      {
+        title : '"Не бей лежачего" - полезные инструменты для быстрого старта разработки проекта на Node.js [Ru]',
+        description : 'В ходе беседы поговорим о некоторых весьма полезных инструментах, для генерации стартового ' +
+        'состояние проекта, автоматической настройки основных компонентов. В общем, как автоматизировать все то, что' +
+        ' каждый раз приходится делать, но не очень хочется'
+      }
+    ],
+    aboutSpeaker : '',
+    socialsRendered : '',
+    socials : [
+      {
+        link : 'https://www.linkedin.com/in/dmitriy-gusev/',
+        fatype : 'linkedin'
+      },
+      {
+        link : 'https://github.com/dogusev"',
+        fatype : 'github'
+      }
+    ]
+  },
+  {
+	image: 'public/img/speakers/alexandra_kalinina.jpg',
+	name : 'Александра Калинина',
+	position : '',
+	company : '',
+	rept : [
+		{
+			title : 'Testing of deployment process to GCP [Ru]',
+			description : ''
+		}
+	],
+	aboutSpeaker : '',
+    socialsRendered : '',
+	socials : []
+  },
+  {
+	image: 'public/img/speakers/dmitry_chirkin.jpg',
+	name : 'Дмитрий Чиркин',
+	position : 'Web Developer',
+	company : 'Nix solutions',
+	rept : [
+		{
+			title : 'PostgreSQL для хипстеров или почему ваш следующий проект должен быть на PostgreSQL [Ru]',
+			description : ''
+		}
+	],
+	aboutSpeaker : '',
+    socialsRendered : '',
+	socials : [
+		{
+			link : 'https://github.com/josser',
+			fatype : 'github'
+		},
+		{
+          link : 'https://facebook.com/josser',
+          fatype : 'facebook'
+        },
+	]
+  }
+];

18dd22153c02b4cd4337976bdb165389e410687e
Artem <as@attractgroup.com>
2018-09-05 18:56:28 +0300

    map

--- resources/js/Modules/GoogleApi.js
+++ resources/js/Modules/GoogleApi.js
@@ -1,6 +1,6 @@
 export let GoogleMapsLoader 	   = require('google-maps');
 GoogleMapsLoader.KEY       = window.CONFIG.GOOGLE_API_KEY || 'AIzaSyDnuSJ4WLgoHZ3io1MRIlheBo7DsNpwUHM';
-GoogleMapsLoader.LIBRARIES = ['places'];
+// GoogleMapsLoader.LIBRARIES = ['places'];
 
 export class GoogleApiModule {
 

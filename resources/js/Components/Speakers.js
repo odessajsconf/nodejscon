4c8fc2d3d709014d35a186a3191ba96e6e4063b5
Artem <as@attractgroup.com>
2018-09-11 17:04:55 +0300

    fix

--- resources/js/Components/Speakers.js
+++ resources/js/Components/Speakers.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
 import { BaseComponent } from '../Base/BaseComponent';
 import { RenderSpeakers } from '../Modules/RenderSpeakers';
-import { MainSpeakersRu } from '../../lang/ru/main-speakers-ru.js';
-import { MainSpeakersEn } from '../../lang/en/main-speakers-en.js';
-import { PrepartySpeakersEn } from '../../lang/en/preparty-speakers-en';
-import { PrepartySpeakersRu } from '../../lang/ru/preparty-speakers-ru';
+import { MainSpeakersRu } from '../../lang/js/ru/main-speakers-ru.js';
+import { MainSpeakersEn } from '../../lang/js/en/main-speakers-en.js';
+import { PrepartySpeakersEn } from '../../lang/js/en/preparty-speakers-en';
+import { PrepartySpeakersRu } from '../../lang/js/ru/preparty-speakers-ru';
 
 export class SpeakersComponent extends BaseComponent {
 

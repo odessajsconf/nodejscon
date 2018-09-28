239aac571c3fa7aa03a25009cffac0e1dbcfb25c
Artem <as@attractgroup.com>
2018-09-28 18:51:11 +0300

    content added

--- resources/lang/js/en/main-schedule-en.js
+++ resources/lang/js/en/main-schedule-en.js
@@ -1,182 +1,188 @@
 export const MainScheduleEn = [
   {
-    time:'9.00-10.00',
-    icon:'profile',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Registration'},
-      {author: '', title: ''}
+    time : '9.00-10.00',
+    icon : 'profile',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Registration'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'10.00-10.40',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-	  {author: 'Dennis Yarmosh', title: 'SQL Indexes and Constraints in depth'},
-      {author: '', title: ''}
+    time : '10.00-10.40',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Max Klymyshyn', title : 'Conflict-free replicated data types: distributed data in detail'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'10.40-10.50',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Break'},
-      {author: '', title: ''}
+    time : '10.40-10.50',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'10.50-11.30',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Max Klymyshyn', title: 'Conflict-free replicated data types: distributed data in detail'},
-      {author: '', title: ''}
+    time : '10.50-11.30',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Dennis Yarmosh', title : 'SQL Indexes and Constraints in depth'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'11.30-11.45',
-    icon:'cup',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Coffee break'},
-      {author: '', title: ''}
+    time : '11.30-11.45',
+    icon : 'cup',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Coffee break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'11.45-12.25',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Yurii Chikhrai', title: 'Eradication of illiteracy in Streams'},
-      {author: 'Max Klymyshyn', title: 'Build blockchain using CRDT and Merkle Trees \[Master Class\]'}
+    time : '11.45-12.25',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Yurii Chikhrai', title : 'Eradication of illiteracy in Streams'},
+      {author : 'Max Klymyshyn', title : 'Build blockchain using CRDT and Merkle Trees \[Master Class\]'}
     ]
   },
   {
-    time:'12.25-13.30',
-    icon:'plate',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Lunch'},
-      {author: '', title: ''}
+    time : '12.25-13.30',
+    icon : 'plate',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Lunch'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'13.30-14.20',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Alexandra Kalinina', title: 'Testing of deployment process to GCP'},
-      {author: '', title: ''}
+    time : '13.30-14.20',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Alexandra Kalinina', title : 'Testing of deployment process to GCP'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'14.20-14.30',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Break'},
-      {author: '', title: ''}
+    time : '14.20-14.30',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'14.30-15.10',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Stanislav Dzhus', title: 'Ways of communication between microservices'},
-      {author: '', title: ''}
+    time : '14.30-15.10',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Stanislav Dzhus', title : 'Ways of communication between microservices'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'15.10-15.25',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Break'},
-      {author: '', title: ''}
+    time : '15.10-15.25',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'15.25-16.05',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Dmitry Chirkin', title: 'PostgreSQL for hipsters or why your next project should be in PostgreSQL'},
-      {author: '', title: ''}
+    time : '15.25-16.05',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Dmitry Chirkin', title : 'PostgreSQL for hipsters or why your next project should be in PostgreSQL'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'16.05-16.15',
-    icon:'cup',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Coffee break'},
-      {author: '', title: ''}
+    time : '16.05-16.15',
+    icon : 'cup',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Coffee break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'16.15-16.55',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Nikolai Boiko', title: 'NodeJS Refactoring: How to kill a Dragon and stay alive'},
-      {author: '', title: ''}
+    time : '16.15-16.55',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Nikolai Boiko', title : 'NodeJS Refactoring: How to kill a Dragon and stay alive'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'16.55-17.10',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Break'},
-      {author: '', title: ''}
+    time : '16.55-17.10',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'17.10-17.50',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-	  {author: 'Roman Sachenko', title: 'NodeJS Security or Blackened is The End'},
-      {author: '', title: ''}
+    time : '17.10-17.50',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Roman Sachenko', title : 'NodeJS Security or Blackened is The End'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'17.50-18.00',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Break'},
-      {author: '', title: ''}
+    time : '17.50-18.00',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Break'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'18.00-18.40',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Dmitriy Gusev', title: '"Don’t hit a man while he’s down" - useful tools for a quick Node.JS project’s development start'},
-      {author: '', title: ''}
+    time : '18.00-18.40',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {
+        author : 'Dmitriy Gusev',
+        title : '"Don’t hit a man while he’s down" - useful tools for a quick Node.JS project’s development start'
+      },
+      {author : '', title : ''}
     ]
   },
   {
-    time:'18.40',
-    icon:'people',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Conference Closing'},
-      {author: '', title: ''}
+    time : '18.40',
+    icon : 'people',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Conference Closing'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'19.00-21.00',
-    icon:'people',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'After-party for real Javascript ninja from the team of organizers NodeUkraine & Codemotion'},
-      {author: '', title: ''}
+    time : '19.00-21.00',
+    icon : 'people',
+    scheduleRendered : '',
+    talks : [
+      {
+        author : '',
+        title : 'After-party for real Javascript ninja from the team of organizers NodeUkraine & Codemotion'
+      },
+      {author : '', title : ''}
     ]
-  },
+  }
 ];
--- resources/lang/js/ru/main-schedule-ru.js
+++ resources/lang/js/ru/main-schedule-ru.js
@@ -1,182 +1,191 @@
 export const MainScheduleRu = [
   {
-    time:'9.00-10.00',
-    icon:'profile',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Регистрация'},
-      {author: '', title: ''}
+    time : '9.00-10.00',
+    icon : 'profile',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Регистрация'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'10.00-10.40',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-	  {author: 'Денис Ярмош', title: 'SQL Indexes and Constraints in depth'},
-      {author: '', title: ''}
+    time : '10.00-10.40',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Максим Климишин', title : 'Conflict-free replicated data types: распределенные данные в деталях'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'10.40-10.50',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Перерыв'},
-      {author: '', title: ''}
+    time : '10.40-10.50',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Перерыв'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'10.50-11.30',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Максим Климишин', title: 'Conflict-free replicated data types: распределенные данные в деталях'},
-      {author: '', title: ''}
+    time : '10.50-11.30',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Денис Ярмош', title : 'SQL Indexes and Constraints in depth'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'11.30-11.45',
-    icon:'cup',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Кофе-брейк'},
-      {author: '', title: ''}
+    time : '11.30-11.45',
+    icon : 'cup',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Кофе-брейк'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'11.45-12.25',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Юрий Чихрай', title: 'Streams - ликвидация безграмотности'},
-      {author: 'Максим Климишин', title: 'Построение блокчейна, используя CRDT и Merkle Trees \[Мастер класс\]'}
+    time : '11.45-12.25',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Юрий Чихрай', title : 'Streams - ликвидация безграмотности'},
+      {author : 'Максим Климишин', title : 'Построение блокчейна, используя CRDT и Merkle Trees \[Мастер класс\]'}
     ]
   },
   {
-    time:'12.25-13.30',
-    icon:'plate',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Обед'},
-      {author: '', title: ''}
+    time : '12.25-13.30',
+    icon : 'plate',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Обед'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'13.30-14.20',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Александра Калинина', title: 'Тестирование деплоймент процесса на GCP'},
-      {author: '', title: ''}
+    time : '13.30-14.20',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Александра Калинина', title : 'Тестирование деплоймент процесса на GCP'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'14.20-14.30',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Перерыв'},
-      {author: '', title: ''}
+    time : '14.20-14.30',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Перерыв'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'14.30-15.10',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Станислав Джус', title: 'Способы коммуникации между микросервисами'},
-      {author: '', title: ''}
+    time : '14.30-15.10',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Станислав Джус', title : 'Способы коммуникации между микросервисами'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'15.10-15.25',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Перерыв'},
-      {author: '', title: ''}
+    time : '15.10-15.25',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Перерыв'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'15.25-16.05',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Дмитрий Чиркин', title: 'PostgreSQL для хипстеров или почему ваш следующий проект должен быть на PostgreSQL'},
-      {author: '', title: ''}
+    time : '15.25-16.05',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {
+        author : 'Дмитрий Чиркин',
+        title : 'PostgreSQL для хипстеров или почему ваш следующий проект должен быть на PostgreSQL'
+      },
+      {author : '', title : ''}
     ]
   },
   {
-    time:'16.05-16.15',
-    icon:'cup',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Кофе-брейк'},
-      {author: '', title: ''}
+    time : '16.05-16.15',
+    icon : 'cup',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Кофе-брейк'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'16.15-16.55',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Николай Бойко', title: 'NodeJS Refactoring: How to kill a Dragon and stay alive'},
-      {author: '', title: ''}
+    time : '16.15-16.55',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Николай Бойко', title : 'NodeJS Refactoring: How to kill a Dragon and stay alive'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'16.55-17.10',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Перерыв'},
-      {author: '', title: ''}
+    time : '16.55-17.10',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Перерыв'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'17.10-17.50',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-	  {author: 'Роман Саченко', title: 'Безопасность в Node.js'},
-      {author: '', title: ''}
+    time : '17.10-17.50',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {author : 'Роман Саченко', title : 'Безопасность в Node.js'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'17.50-18.00',
-    icon:'clock',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Перерыв'},
-      {author: '', title: ''}
+    time : '17.50-18.00',
+    icon : 'clock',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Перерыв'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'18.00-18.40',
-    icon:'sheet',
-    scheduleRendered: '',
-    talks: [
-      {author: 'Дмитрий Гусев', title: '"Не бей лежачего" - полезные инструменты для быстрого старта разработки проекта на Node.js'},
-      {author: '', title: ''}
+    time : '18.00-18.40',
+    icon : 'sheet',
+    scheduleRendered : '',
+    talks : [
+      {
+        author : 'Дмитрий Гусев',
+        title : '"Не бей лежачего" - полезные инструменты для быстрого старта разработки проекта на Node.js'
+      },
+      {author : '', title : ''}
     ]
   },
   {
-    time:'18.40',
-    icon:'people',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'Закрытие конференции'},
-      {author: '', title: ''}
+    time : '18.40',
+    icon : 'people',
+    scheduleRendered : '',
+    talks : [
+      {author : '', title : 'Закрытие конференции'},
+      {author : '', title : ''}
     ]
   },
   {
-    time:'19.00-21.00',
-    icon:'people',
-    scheduleRendered: '',
-    talks: [
-      {author: '', title: 'After-party для настоящих Javascript ninja от команды организаторов NodeUkraine & Codemotion'},
-      {author: '', title: ''}
+    time : '19.00-21.00',
+    icon : 'people',
+    scheduleRendered : '',
+    talks : [
+      {
+        author : '',
+        title : 'After-party для настоящих Javascript ninja от команды организаторов NodeUkraine & Codemotion'
+      },
+      {author : '', title : ''}
     ]
-  },
+  }
 ];

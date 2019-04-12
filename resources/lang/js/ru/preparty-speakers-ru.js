32a69c6f9ccb3b73267c9b8c0ee87c0cd9b8b3a2
BondarDmitry <bondarconstr@gmail.com>
2019-04-12 18:01:39 +0300

    ticket-12.04

--- resources/lang/js/ru/preparty-speakers-ru.js
+++ resources/lang/js/ru/preparty-speakers-ru.js
@@ -54,5 +54,65 @@ export const PrepartySpeakersRu = [
                 fatype : 'link'
             },
         ]
+    },
+    {
+        image : '../public/img/speakers/tanya-butenko.png',
+        name : 'Tanya Butenko',
+        speakerPos: [
+            {
+                position: 'Software engineer',
+                company: 'Hireup'
+
+            },
+            {
+                position: 'Founder and CEO',
+                company: 'Muses Code JS Inc'
+            },
+        ],
+
+        place : 'Sydney, Australia',
+        rept : [
+            {
+                title : 'Lift me up. Mentorship 101 <br> Duration - 1 hours ',
+                description :
+                    '<p class="text-bold m-b-5">DESCRIPTION</p>' +
+                    '<p class="m-t-0">Mentorship, coaching, sponsorship - it looks like we hear these words more and more in IT communities, at working place and on events. So what is a mentorship and how to be good at it? Often when we start mentor/mentee interaction we might notice that these "simple" soft skills are not so simple and not so soft. This talk will give you great insight on mentor\'s etiquette and how to gain most being a mentor. It also gives a great understanding of mentor-mentee relations to the mentee.</p>' +
+                    '<p class="text-bold m-b-5">In-depth</p>' +
+                    '<ul class="m-t-0">\n' +
+                    '  <li> what is mentorship, coaching, sponsorship and what\'s the difference </li>\n' +
+                    '  <li> why do we need mentorship</li>\n' +
+                    '  <li> why you should mentor and what you will gain from it</li>\n' +
+                    '  <li> what you should do and never do as a mentore</li>\n' +
+                    '  <li> how to find and approach a mentor for yourself</li>\n' +
+                    '  <li> what you should do and never do as a mentee</li>\n' +
+                    '  <li> everything is shown with real life examples and explanations</li>\n' +
+                    '</ul>' +
+                    '<p class="text-bold m-b-5">WHO SHOULD ATTEND</p>' +
+                    '<p class="m-t-0">anyone who is interested in any side of mentorship/coaching</p>'+
+                    '<p class="text-bold m-b-5">ADDITIONAL INFO</p>' +
+                    '<p class="m-t-0">partially this talk was made for the last 3 years for mentors of Muses Code JS, it was also partially presented on Girls Geek Dinner in Sydney and communicated on many panels around Sydney</p>' +
+                    '<p class="text-bold m-b-5">WHY AM I QUALIFIED TO TALK ABOUT IT?</p>' +
+                    '<p class="m-t-0">Coming up with teacher\'s background and degree, being very interested in psychology, running and mentoring on more than 100 coding workshops, starting a new career in web-development from scratch, mentoring and coaching a handful of junior devs puts me in a unique position to be able to talk about good and bad mentorship practices. Having a big experience of being a mentee and having different mentors gave me an even deeper understanding of how to make mentor/mentee relations successful.</p>'
+
+            }
+        ],
+        aboutSpeaker :
+            'Coming up with teacher\'s background and degree, being very interested in psychology, running and mentoring on more than 100 coding workshops, starting a new career in web-development from scratch, mentoring and coaching a handful of junior devs puts me in a unique position to be able to talk about good and bad mentorship practices. Having a big experience of being a mentee and having different mentors gave me an even deeper understanding of how to make mentor/mentee relations successful.',
+        title : 'Lift me up. Mentorship 101',
+        socialsRendered : '',
+        socials : [
+            {
+                link: 'https://twitter.com/ButenkoMe',
+                fatype: 'twitter'
+            },
+            {
+                link: 'https://github.com/ButenkoT',
+                fatype: 'github'
+            },
+            {
+                link: 'https://musescodejs.org',
+                fatype: 'link'
+            }
+        ]
     }
 ];
